a0bd1ee813bbbf3581ff51b7fbabb0fae51091ae
DANIEL KATAMA MWANZIA <91879978+katamadaniel@users.noreply.github.com>
2025-05-17 04:04:10 +0300

    Removed load spinner from PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ isAuthenticated, children }) => {
-  if (isAuthenticated === null) return <LoadingSpinner />;
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
